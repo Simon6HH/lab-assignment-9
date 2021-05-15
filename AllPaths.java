81ec3fa19076b060b1adaae2801ff1e0e1a53ee9
Simon Hackbert <60202596+Simon6HH@users.noreply.github.com>
2021-05-14 21:30:13 -0500

    Add files via upload

--- AllPaths.java
+++ AllPaths.java
@@ -2,60 +2,29 @@ import java.util.ArrayList;
 
 public class AllPaths {
 
-    int[][] graph;
-    ArrayList<ArrayList<Integer>> paths = new ArrayList<>();
+    int[][] graphs; /** 2d graphs initialization **/
+    ArrayList<ArrayList<Integer>> paths = new ArrayList<>(); /** arraylist paths **/
 
-    /*creates an adjacency matrix of sorts where
-     * the row is the "node" and the column is what it points to
-     * where 1 means it points to it and 0 means it does not
-     */
-    public int[][] createGraph(int[][] input){
-        graph = new int[input.length][input.length];
-        for(int i = 0; i < input.length; i++){
-//            graph[i] = new int[input[i].length];
-            for(int j = 0; j < input[i].length; j++){
-                graph[i][input[i][j]] = 1;
+    public int[][] create(int[][] input) { /** adjacency graph creation **/
+        graphs = new int[input.length][input.length];
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input[i].length; j++) {
+                graphs[i][input[i][j]] = 1; /** if 1, it points, if 0, it doesn't **/
             }
         }
-        return graph;
+        return graphs;
     }
 
-    /*
-     *make sure paths is clear since its global to start a new graph
-     * create the graph datastructure as a 2d array
-     * try path from 0
-     * return int[][] of all possible paths from 0 -> final index
-     */
-    public int[][] allPaths(int[][] input) {
-        paths.clear();
-        graph = createGraph(input);
-        ArrayList prevPath = new ArrayList<>();
-        tryPath(prevPath,0);
-        return convertToIntArr(paths);
-    }
-
-    //convert to int[][](couldn't find a method that worked for me so manual)
-    public int[][] convertToIntArr(ArrayList<ArrayList<Integer>> arr){
-        int[][] arrPaths = new int[paths.size()][];
-        for(int i = 0; i < paths.size(); i++){
-            arrPaths[i] = new int[paths.get(i).size()];
-            for(int j = 0; j < paths.get(i).size(); j++) {
-                arrPaths[i][j] = paths.get(i).get(j);
-            }
-        }
-        return arrPaths;
-    }
-
-    public boolean tryPath(ArrayList<Integer> prevPath, int current){
-        ArrayList<Integer> path = (ArrayList)prevPath.clone(); //creates a clone of the previous path to continue
+    public boolean tryPath(ArrayList<Integer> previous, int current) {
+        ArrayList<Integer> path = (ArrayList)previous.clone(); /** clone of path **/
         path.add(current);
-        for(int i = 0; i < graph[current].length; i++){
-            if(graph[current][i] == 1){
-                if(i == graph.length-1) {//if we reach the target (end of path)
+        for (int i = 0; i < graphs[current].length; i++) {
+            if (graphs[current][i] == 1) {
+                if (i == graphs.length - 1) { /** path ends if its -1  **/
                     path.add(i);
                     paths.add(path);
                     return true;
-                }else {//else go again at the next path
+                } else {
                     tryPath(path, i);
                 }
             }
@@ -63,10 +32,25 @@ public class AllPaths {
         return false;
     }
 
-    private void print(int[][] arr){
-        for(int row = 0; row < arr.length; row++) {
-            for (int i = 0; i < arr[row].length; i++) {
-                System.out.print(" " + arr[row][i]);
+    public int[][] allPaths(int[][] input) {
+        paths.clear(); /** need to clear because its a global variable, need to clear for this check function **/
+        graphs = create(input); /** creates graph with the input **/
+        ArrayList previous = new ArrayList<>();
+        tryPath(previous,0);
+        int[][] arrPaths = new int[paths.size()][]; /** new array path 2d array which converts the integer array **/
+        for (int i = 0; i < paths.size(); i++) {
+            arrPaths[i] = new int[paths.get(i).size()];
+            for (int j = 0; j < paths.get(i).size(); j++) {
+                arrPaths[i][j] = paths.get(i).get(j);
+            }
+        }
+        return arrPaths; /** returns 2d array with 0 and -1 vertex's **/
+    }
+
+    private void toString(int[][] arr) { /** prints the graph in row column form, binary sort **/
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(" " + arr[i][j]);
             }
             System.out.println();
         }
@@ -74,28 +58,21 @@ public class AllPaths {
 
     public static void main(String[] args){
 
-        int[][] test1 = new int[][] {{1,2},{3},{3},{}};
-
-        int[][] test2 = new int[][] {{4,3,1},{3,2,4},{3},{4},{}};
-
-        int[][] test3 = new int[][] {{1},{}};
-
+        int[][] TEST1 = new int[][] {{1,2},{3},{3},{}};
+        int[][] TEST2 = new int[][] {{1},{}};
+        int[][] TEST3 = new int[][] {{4,3,1},{3,2,4},{3},{4},{}};
         AllPaths test = new AllPaths();
 
-        int[][] paths1 = test.allPaths(test1);
+        int[][] path1 = test.allPaths(TEST1);
+        System.out.println("TEST 1 : ");
+        test.toString(path1);
 
-        System.out.println("TEST1:");
-        test.print(paths1);
+        int[][] path2 = test.allPaths(TEST2);
+        System.out.println("TEST 2 : ");
+        test.toString(path2);
 
-        int[][] paths2 = test.allPaths(test2);
-
-        System.out.println("TEST2:");
-        test.print(paths2);
-
-        int[][] paths3 = test.allPaths(test3);
-
-        System.out.println("TEST3:");
-        test.print(paths3);
-
+        int[][] path3 = test.allPaths(TEST3);
+        System.out.println("TEST 3 : ");
+        test.toString(path3);
     }
 }
